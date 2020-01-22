76178639cfafe7c5850cb437daf4747494e16d7f
Eddie <eddie.muss@archipinion.com>
2020-01-22 15:00:53 +0100

    remove console.log

--- src/api/CategoryApi.js
+++ src/api/CategoryApi.js
@@ -4,7 +4,6 @@ const URL = 'http://localhost:3000/api/categories';
 
 export const fetchAllCategories = async (jwt) => {
   const token = bearerToken(jwt);
-  console.log('token ', token);
   const parameters = Parameter.get(token);
   const categories = fetchData(URL, parameters);
   return categories;
