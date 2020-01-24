62bd6347e51420608b8b8611936dd6f0d85007fe
TerminatorMP <kingmarcel98@gmx.de>
2020-01-24 12:55:46 +0100

    added Header for Home SEite

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -2,12 +2,14 @@
 import React from 'react';
 
 import HomePageMetaTags from './HomePageMetaTag';
+import HomeHeader from '../../components/HomeHeader/HomeHeader';
 import { isLoaded } from '../../helper/helper';
 import Categories from '../../components/Categories/Categories';
 
 const HomePage = ({ categories }) => (
   <div>
     <HomePageMetaTags />
+    <HomeHeader />
     {isLoaded(categories, <Categories categories={categories} />)}
   </div>
 );
