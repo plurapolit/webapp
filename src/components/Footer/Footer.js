10277451afb54bd5f10f98b89af49b8211d76904
Eddie <eddie.muss@archipinion.com>
2020-01-24 14:01:31 +0100

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,13 @@ import React from 'react';
 import styles from './Footer.module.scss';
 
 const Footer = () => (
-  <div>
-    Footer
-  </div>
+  <footer className={styles["footer"]}>
+    <ul className={styles["container"]}>
+      <li className={styles["item"]}>Datenschutzerklärung</li>
+      <li className={styles["item"]}>Impressum</li>
+      <li className={styles["item"]}>Nutzungsbedingungen</li>
+    </ul>
+  </footer>
 );
 
 export default Footer;
