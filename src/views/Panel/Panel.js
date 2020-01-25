1094536da7016dd57771706e5c7d564cd69cc374
Eddie <eddie.muss@archipinion.com>
2020-01-25 15:29:45 +0100

    Add components to panel

--- src/views/Panel/Panel.js
+++ src/views/Panel/Panel.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
+import styles from './Panel.module.scss';
+import Text from '../../components/Text/Text';
+import ContentWrapper from '../../components/ContentWrapper/ContentWrapper';
+
+const IMAGEROOTURL = process.env.REACT_APP_ROOT_IMAGE_URL;
 
 const Panel = ({ panel }) => {
   console.log('panel ', panel);
+  const refPanel = useRef(undefined);
 
-  return <p>hello there</p>;
+  useEffect(() => {
+    const color = '#0000CC';
+    refPanel.current.style.setProperty('--color', color);
+  }, []);
+
+  return (
+    <div ref={refPanel} className={styles["panel"]}>
+      <img
+        src={`${IMAGEROOTURL}/${panel.category_avatar_key}`}
+        alt={panel.category.name}
+        className={styles["image"]}
+      />
+      <div className={styles["container"]}>
+        <ContentWrapper>
+          <div className={styles["headline"]}>
+            {panel.category.name}
+          </div>
+        </ContentWrapper>
+        <div className={styles["bg"]}>
+          <ContentWrapper>
+            <Text headline={panel.panel.title}>
+              {panel.panel.description}
+            </Text>
+          </ContentWrapper>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Panel;
