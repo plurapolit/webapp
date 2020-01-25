9b8ac10533c9238a826a1acfd957742ed270e3d2
Eddie <eddie.muss@archipinion.com>
2020-01-25 18:06:22 +0100

    update page not found

--- src/views/PageNotFound/PageNotFound.js
+++ src/views/PageNotFound/PageNotFound.js
@@ -6,22 +6,20 @@ import ContentWrapper from '../../components/ContentWrapper/ContentWrapper';
 import Text from '../../components/Text/Text';
 import Button from '../../components/Button/Button';
 
-const PageNotFound = () => {
-  return (
-    <>
-      <div className={styles["page-not-found"]}>
-        <ContentWrapper>
-          <Text>
-            Auf dieser Webseite diskutiert heute leider keiner,
-            aber besuch doch eine unserer anderen Seiten.
-          </Text>
-          <Link to="/">
-            <Button>Startseite</Button>
-          </Link>
-        </ContentWrapper>
-      </div>
-    </>
-  );
-};
+const PageNotFound = () => (
+  <>
+    <div className={styles["page-not-found"]}>
+      <ContentWrapper>
+        <Text>
+          Auf dieser Webseite diskutiert heute leider keiner,
+          aber besuch doch eine unserer anderen Seiten.
+        </Text>
+        <Link to="/">
+          <Button>Startseite</Button>
+        </Link>
+      </ContentWrapper>
+    </div>
+  </>
+);
 
 export default PageNotFound;
