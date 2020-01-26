56bf1d786610dcc565d7ab356cd1e60f1011f99d
Eddie <eddie.muss@archipinion.com>
2020-01-26 21:58:25 +0100

    use color for api

--- src/components/PanelList/PanelList.js
+++ src/components/PanelList/PanelList.js
@@ -4,17 +4,16 @@ import styles from './PanelList.module.scss';
 import PanelCard from '../PanelCard/PanelCard';
 
 const PanelList = ({ data }) => {
-
+  const URL = process.env.REACT_APP_ROOT_IMAGE_URL;
   const list = data.map(({ panel }) => {
-    const url = 'https://i.picsum.photos/id/730/700/500.jpg';
-    const c = '#000';
+    const url = `${URL}/lwm4qgq7dij2uy2zppuaypwwijqt`;
     return (
       <PanelCard
         key={panel.id}
         title={panel.title}
         shortTitle={panel.short_title}
         imageUrl={url}
-        color={c}
+        color={panel.font_color}
         slug={panel.slug}
       />
     );
