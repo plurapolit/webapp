ee3372435872fe63d6809e2a8ec8ce782f43293d
TerminatorMP <kingmarcel98@gmx.de>
2020-01-29 19:40:51 +0100

    edited Loader Site

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { ScaleLoader } from 'react-spinners';
 
+import styles from './Loader.module.scss';
+
 const Loader = () => (
-  <ScaleLoader
-    size={150}
-    color={"#123abc"}
-    loading={true}
-  />
+  <div className={styles["container"]}>
+    <ScaleLoader
+      height={140}
+      width={20}
+      color={"#123abc"}
+      loading={true}
+    />
+  </div>
 );
 
 export default Loader;
