1e50f831268c48aa488b1f8b7b2e36e5fe924db4
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-02-02 22:15:01 +0100

    Style the audio recorder

--- src/components/AudioRecorder/AudioRecorder.js
+++ src/components/AudioRecorder/AudioRecorder.js
@@ -5,9 +5,10 @@ import Button from '../Button/Button';
 import Recorder from '../../helper/Recorder';
 
 import styles from './AudioRecorder.module.scss';
+import microphoneButton from '../../media/images/microphone.svg';
 
 const AudioRecorder = ({
-  userId, setFileLink, setDuration, nextPage,
+  setFileLink, setDuration, nextPage,
 }) => {
   const [audio, setAudio] = useState(null);
   const [isRecording, setIsRecording] = useState(false);
@@ -78,6 +79,7 @@ const AudioRecorder = ({
         <Button onClick={() => stopMicrophone()}>
           Aufname stoppen
         </Button>
+        <div className={styles["step-index"]}>Schritt 2 von 4</div>
       </div>
     );
   }
@@ -85,24 +87,33 @@ const AudioRecorder = ({
   if (blobURL) {
     return (
       <div className={styles["container"]}>
-        <audio src={blobURL} controls />
+        <div className={styles["recording-headline"]}>Zufrieden?</div>
+        <div className={styles["recorded-audio"]}>
+          <audio src={blobURL} controls />
+        </div>
         <div className={styles["button-container"]}>
-          <Button onClick={deleteCachedAudio}>
-            Löschen
-          </Button>
+          <div className={styles["delete-audio"]} onClick={deleteCachedAudio}>
+            Neu aufnehmen
+          </div>
           <Button onClick={send}>
-            Abschicken
+            Weiter
           </Button>
         </div>
+        <div className={styles["step-index"]}>Schritt 3 von 4</div>
       </div>
     );
   }
 
   return (
     <div className={styles["container"]}>
-      <Button onClick={startRecording}>
-        Aufname starten
-      </Button>
+      <div
+        className={styles['recording-button']}
+        onClick={startRecording}
+      >
+        <img alt="icon" src={microphoneButton} className={styles['recording-button-img']} />
+        <div className={styles['recording-button-text']}>Aufnahme starten</div>
+      </div>
+      <div className={styles["step-index"]}>Schritt 2 von 4</div>
     </div>
   );
 };
