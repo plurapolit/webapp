3ea7221ea1679700d3222aaceff2dffea06ae699
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-02-03 00:28:59 +0100

    panel and audio recorder

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,8 +4,6 @@ import ReactModal from 'react-modal';
 const Modal = ({
   children, isOpen, onClose, label = 'PluraPolit', style = {},
 }) => (
-  // TODO: Stell sicher, dass das Modal auf allen screens gut aussehen.
-
   <ReactModal
     isOpen={isOpen}
     onRequestClose={onClose}
