29084f57ee37421b10c4e83a55abb7af969d75a9
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-02-04 19:43:33 +0100

    Implement Matomo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 import Store from './layouts/Store/Store';
 import Routes from './Routes';
 import ScrollToTop from './helper/ScrollToTop';
+import Piwik from './helper/Piwik';
 
+const history = createBrowserHistory();
 
 const App = () => (
   <Store>
-    <Router>
+    <Router history={Piwik.connectToHistory(history)}>
       <ScrollToTop />
       <Routes />
     </Router>
---
+++ src/helper/Piwik.js
@@ -0,0 +1,25 @@
+import ReactPiwik from 'react-piwik';
+
+let siteId;
+
+switch (process.env.REACT_APP_ENV) {
+  case 'development':
+    siteId = 3;
+    break;
+  case 'staging':
+    siteId = 2;
+    break;
+  case 'production':
+    siteId = 1;
+    break;
+  default:
+    siteId = null;
+}
+
+const Piwik = new ReactPiwik({
+  url: 'https://plurapolit.matomo.cloud',
+  siteId,
+  trackErrors: true,
+});
+
+export default Piwik;
