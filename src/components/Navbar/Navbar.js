fbb68fc7e67709e650d700e4081156c64c3ea187
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-02-11 14:24:09 +0100

    Remove long comment

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// import LogoWhite from "./images/PluraPolitLogowhite.png";
 import LogoBlack from "./images/PluraPolitLogoblack.png";
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
 import styles from "./Navbar.module.scss";
@@ -10,36 +9,11 @@ import SignOutButton from "../SignOutButton/SignOutButton";
 import StoreContext from "../../layouts/Store/StoreContext";
 
 const Navbar = ({ user }) => {
-  // const [isAtTop, setIsAtTop] = useState(false);
-  // let customStyle;
-
-  // const handleScroll = (event) => {
-  //   const scrollTop = event.currentTarget.scrollY;
-  //   if (scrollTop > 100) {
-  //     setIsAtTop(false);
-  //   }
-  //   if (scrollTop <= 100) {
-  //     setIsAtTop(true);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener('scroll', handleScroll);
-  // }, []);
-
-  // if (isAtTop) {
-  //   customStyle = {
-  //     nav: styles["navbar_top"],
-  //     item: styles["item_top"],
-  //     logo: LogoWhite,
-  //   };
-  // } else {
   const customStyle = {
     nav: styles["navbar"],
     item: styles["item"],
     logo: LogoBlack,
   };
-  // }
 
   return (
     <nav className={customStyle.nav}>
