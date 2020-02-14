9a86ba65ff2bb231a8549e98cb584b982b30fd48
eddie <eddimuss@web.de>
2020-02-14 16:35:22 +0100

    create first test for audio recorder

---
+++ src/components/AudioRecorder/AudioRecorder.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import AudioRecorder from "./AudioRecorder";
+
+const setup = (propOverrides) => {
+  const props = {
+    setFileLink: jest.fn(),
+    setDuration: jest.fn(),
+    nextPage: jest.fn(),
+    ...propOverrides,
+  };
+  const wrapper = shallow(
+    <AudioRecorder
+      setFileLink={props.setFileLink}
+      setDuration={props.setDuration}
+      nextPage={props.nextPage}
+    />,
+  );
+  return {
+    wrapper,
+    props,
+  };
+};
+
+describe("<AudioRecorder />", () => {
+  it("should render button for recording", () => {
+    const { wrapper } = setup();
+    expect(wrapper.find(".recording-button")).toHaveLength(1);
+  });
+
+  // it("should render stop button while recording", () => {
+  //   const { wrapper } = setup();
+  //   wrapper.find(".recording-button").simulate("click");
+  //   console.log(wrapper.debug());
+  //   expect(wrapper);
+  // });
+});
