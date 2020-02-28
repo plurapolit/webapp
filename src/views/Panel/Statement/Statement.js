1a9c3039fab5d34dd78059bf8c24179f669afa2f
TerminatorMP <kingmarcel98@gmx.de>
2020-02-28 18:18:15 +0100

    Statement refactored

--- src/views/Panel/Statement/Statement.js
+++ src/views/Panel/Statement/Statement.js
@@ -1,56 +1,18 @@
 import React, { useState, useEffect } from "react";
-import moment from "moment-with-locales-es6";
+import { If } from "react-if";
 
 import styles from "./Statement.module.scss";
 
-import { usePlayerContext } from "../../../contexts/PlayerContext/PlayerContext";
-import Button, { ButtonStyle } from "../../../components/Button/Button";
-import audioWave from "../../../assets/images/sound-wave.svg";
-import playButton from "../../../assets/images/play.svg";
 import PanelComments from "../PanelComments/PanelComments";
-import TwitterButton from "../../../components/TwitterButton/TwitterButton";
-import Time from "../../../helper/TimeHelper";
+import StatementHeader from "./StatementHeader/StatementHeader";
+import StatementControls from "./StatementControls/StatementControls";
 
 const Statement = ({
   expert,
   panelTitle,
 }) => {
   const [commentsAreOpen, setCommentsAreOpen] = useState(false);
-  const { setAudio } = usePlayerContext();
-
-  const IMAGEROOTURL = process.env.REACT_APP_BUCKET_URL;
-
-  const avatar = () => {
-    const image = (
-      <img
-        src={`${IMAGEROOTURL}/${expert.user_avatar_key}`}
-        alt={expert.user.full_name}
-        className={styles["expert-card-image"]}
-      />
-    );
-
-    if (expert.user.website_link) {
-      return (
-        <a href={expert.user.website_link} target="_blank" rel="noopener noreferrer">
-          {image}
-        </a>
-      );
-    }
-    return image;
-  };
-
   const { number_of_comments: commentNumber } = expert;
-  const numberOfComments = () => {
-    if (commentNumber === 0) {
-      return "Kommentieren";
-    }
-
-    if (commentNumber === 1) {
-      return "1 Kommentar";
-    }
-
-    return `${commentNumber} Kommentare`;
-  };
 
   useEffect(() => {
     const openCommentsIfCommentsAreAvailable = () => {
@@ -61,88 +23,24 @@ const Statement = ({
     openCommentsIfCommentsAreAvailable();
   }, [commentNumber]);
 
-  const audioDuration = () => moment
-    .utc(
-      moment
-        .duration(expert.statement_audio_file.duration_seconds, "seconds")
-        .asMilliseconds(),
-    )
-    .format("mm:ss");
-
   const toggleComments = () => {
     setCommentsAreOpen(!commentsAreOpen);
   };
 
   return (
-    <div className={styles["expert-wrapper"]}>
-      <div className={styles["expert-card"]}>
-        <div className={styles["expert-card-header"]}>
-          <div className={styles["expert-card-header-left"]}>
-            {avatar()}
-            <div className={styles["expert-card-user"]}>
-              <div className={styles["expert-card-name-wrapper"]}>
-                <div className={styles["expert-card-name"]}>
-                  {expert.user.full_name}
-                </div>
-              </div>
-              <div className={styles["expert-card-organisation"]}>
-                {expert.organisation.name}
-              </div>
-              <div className={styles["expert-card-organisation"]}>
-                {expert.user.biography}
-              </div>
-            </div>
-          </div>
-          <TwitterButton handle={expert.user.twitter_handle} />
+    <div className={styles["statement-comment-container"]}>
+      <div className={styles["card"]}>
+        <StatementHeader expert={expert} />
+        <div className={styles["quote"]}>
+          {`"${expert.statement.quote}"`}
         </div>
-        <div className={styles["expert-card-statement"]}>
-          <div className={styles["expert-card-quote-text"]}>
-            {`"${expert.statement.quote}"`}
-          </div>
-        </div>
-        <div className={styles["expert-card-controls"]}>
-          <Button
-            buttonStyle={ButtonStyle.COMMENT}
-            onClick={() => toggleComments()}
-          >
-            {numberOfComments(expert)}
-          </Button>
-          <div className={styles["expert-card-nav"]}>
-            <div className={styles["expert-card-nav_info-container"]}>
-              <span>{Time.getDateOrTime(expert.statement.created_at)}</span>
-              <div className={styles["expert-card-nav-time-wrapper"]}>
-                <img
-                  src={audioWave}
-                  alt={expert.user.full_name}
-                  className={styles["expert-card-nav-img"]}
-                />
-                <div className={styles["expert-card-nav-time"]}>
-                  {audioDuration(expert)}
-                </div>
-              </div>
-            </div>
-            <button
-              type="button"
-              className={styles["expert-card-nav-play"]}
-              onClick={() => {
-                setAudio(
-                  expert.statement_audio_file.file_link,
-                  expert.user.full_name,
-                  expert.statement.id,
-                  panelTitle,
-                );
-              }}
-            >
-              <img
-                src={playButton}
-                alt={expert.user.full_name}
-                className={styles["expert-card-nav-play-img"]}
-              />
-            </button>
-          </div>
-        </div>
+        <StatementControls
+          expert={expert}
+          panelTitle={panelTitle}
+          toggleComments={toggleComments}
+        />
       </div>
-      {commentsAreOpen && (
+      <If condition={commentsAreOpen}>
         <PanelComments
           toggleComments={toggleComments}
           statementId={expert.statement.id}
@@ -150,7 +48,7 @@ const Statement = ({
           statementDate={expert.statement.created_at}
           panelTitle={panelTitle}
         />
-      )}
+      </If>
     </div>
   );
 };
