c347ba7f428ddb6456175c5793baa17ae975d927
eddie <eddimuss@web.de>
2020-03-09 17:29:14 +0100

    ignore eslint errors because unused variables are needed for cypress to work

--- cypress/integration/RecordComment.test.js
+++ cypress/integration/RecordComment.test.js
@@ -5,9 +5,11 @@ import { pageUrl, user } from "../Helper";
 describe("Record Comment", () => {
   it("should successfully record a statement and save it", () => {
     cy.visit(pageUrl);
+    // eslint-disable-next-line no-unused-vars
     cy.get("[data-test=statement]").first().within(($Statement) => {
       cy.get("[data-test=create-comment-button]").should("have.length", 1).click();
     });
+    // eslint-disable-next-line no-unused-vars
     cy.get("form").within(($form) => {
       cy.get('[type="email"]').type(user.email);
       cy.get('[type="password"]').type(user.password);
@@ -19,6 +21,7 @@ describe("Record Comment", () => {
     cy.wait(1000);
     cy.get("[data-test=shop-recording-button]").should("have.length", 1).click();
     cy.contains("Weiter").click();
+    // eslint-disable-next-line no-unused-vars
     cy.get("form").within(($form) => {
       cy.get("textarea").type("Just a test message");
     });
