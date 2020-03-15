a2a5060a1df739b08387a74ecaefd4a5455f4655
eddie <eddimuss@web.de>
2020-03-15 14:04:20 +0100

    add a new spinner from react-spinner

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { ClipLoader } from "react-spinners";
+import { ClipLoader, FadeLoader } from "react-spinners";
 
 import styles from "./Loader.module.scss";
 
 const Loader = () => (
   <div className={styles["container"]}>
     <ClipLoader size={110} css={{ borderWidth: "1rem" }} color="#123abc" loading />
+  </div>
+);
+
+export const Spinner = () => (
+  <div>
+    <FadeLoader />
   </div>
 );
 
