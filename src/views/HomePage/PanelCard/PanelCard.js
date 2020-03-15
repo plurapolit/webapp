4ba0b60f25039a2302e3b6bfd37e94bc03c6023f
eddie <eddimuss@web.de>
2020-03-15 14:09:37 +0100

    implement changes in current setAudioList calls

--- src/views/HomePage/PanelCard/PanelCard.js
+++ src/views/HomePage/PanelCard/PanelCard.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { If, Then, Else } from "react-if";
 
+import { useTransition } from "../../../helper/CustomHookHelper";
+import { Spinner } from "../../../components/Loader/Loader";
+import PanelApi from "../../../api/PanelApi";
 import ExpertsList from "../ExpertsList/ExpertsList";
 import playerImage from "../../../assets/images/play.svg";
 import styles from "./PanelCard.module.scss";
+import { useStoreContext } from "../../../contexts/StoreContext/StoreContext";
+import { usePlayerContext } from "../../../contexts/PlayerContext/PlayerContext";
+import { createAudioTrackListFromExpertStatements } from "../../../helper/AudioTrackHelper";
 
 const PanelCard = ({
   title, imageUrl, color, shortTitle, slug, experts,
@@ -12,11 +19,28 @@ const PanelCard = ({
     "--url": `url(${imageUrl})`,
     "--color": `${color}`,
   };
+  const { getPanelIdBySlug } = useStoreContext();
+  const { setAudioTrackList } = usePlayerContext();
+  const [startTransition, isPending] = useTransition();
+
+  const playAllAudioTracks = async () => {
+    const panelId = getPanelIdBySlug(slug);
+    const panel = await startTransition(() => PanelApi.fetchPanelById(panelId));
+    const audios = createAudioTrackListFromExpertStatements(panel.expert_statements, shortTitle);
+    setAudioTrackList(audios);
+  };
 
   return (
     <div className={styles["panel-card"]}>
-      <div className={styles["playAllButton"]}>
-        <img src={playerImage} alt="play" />
+      <div className={styles["playAllButton"]} onClick={playAllAudioTracks}>
+        <If condition={isPending}>
+          <Then>
+            <Spinner />
+          </Then>
+          <Else>
+            <img src={playerImage} alt="play" />
+          </Else>
+        </If>
       </div>
       <Link to={`${slug}`}>
         <div className={styles["question-banner"]} style={customStyle} data-test="panel-card">
--- src/views/Panel/PlayAllButton/PlayAllButton.js
+++ src/views/Panel/PlayAllButton/PlayAllButton.js
@@ -4,33 +4,14 @@ import { usePlayerContext } from "../../../contexts/PlayerContext/PlayerContext"
 import Button from "../../../components/Button/Button";
 import styles from "./PlayAllButton.module.scss";
 import playButton from "../../../assets/images/play.svg";
+import { createAudioTrackListFromExpertStatements } from "../../../helper/AudioTrackHelper";
 
 const PlayAllButton = ({
   expertStatements,
   panelTitle,
 }) => {
   const { setAudioTrackList } = usePlayerContext();
-  const audios = [];
-  expertStatements.forEach((statement) => {
-    const { intro } = statement;
-    if (intro) {
-      const introStatement = {
-        audioFile: intro.audio_file_link,
-        author: statement.user.full_name,
-        statementId: statement.statement.id,
-        panelTitle,
-        isIntro: true,
-      };
-      audios.push(introStatement);
-    }
-    const audioStatement = {
-      audioFile: statement.statement_audio_file.file_link,
-      author: statement.user.full_name,
-      statementId: statement.statement.id,
-      panelTitle,
-    };
-    audios.push(audioStatement);
-  });
+  const audios = createAudioTrackListFromExpertStatements(expertStatements, panelTitle);
 
   const handleClick = () => {
     setAudioTrackList(audios);
