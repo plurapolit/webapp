a6ab4746cf4497084f5928278ff7a61f501dffe2
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-04-02 10:27:04 +0200

    Update Supporter Images

--- src/views/HomePage/Supporters/Supporters.js
+++ src/views/HomePage/Supporters/Supporters.js
@@ -1,24 +1,28 @@
 import React from "react";
 
 import styles from "./Supporters.module.scss";
-import LPB from "./images/lpb-hh.jpg";
-import StartupIncubator from "./images/startup-incubator.jpg";
-import Partner1 from "./images/sib-partner1.jpg";
+import LPB from "./images/logo-lzpb-hamburg.png";
+import StartupIncubator from "./images/2_HWR_Logo.png";
+import SIBLogo from "./images/1_SIB_Logo.png";
+import Partner1 from "./images/3_be_Berlin_Logo.png";
 import Partner2 from "./images/sib-partner2.jpg";
-import Partner3 from "./images/sib-partner3.jpg";
+import Partner3 from "./images/5_EU_Sozialfonds_R_Logo.png";
 
 const Supporters = () => (
   <div className={styles["container"]}>
     <div className={styles["image-wrapper"]}>
-      <img
-        className={styles["image"]}
-        src={StartupIncubator}
-        alt="sample-logo"
-      />
-    </div>
-    <div className={styles["image-wrapper"]}>
       <img className={styles["image"]} src={LPB} alt="sample-logo" />
     </div>
+    <a href="https://www.startup-incubator.berlin/">
+      <div className={styles["image-wrapper"]}>
+        <img className={styles["image"]} src={SIBLogo} alt="sample-logo" />
+      </div>
+    </a>
+    <a href="http://www.hwr-berlin.de/">
+      <div className={styles["image-wrapper"]}>
+        <img className={styles["image"]} src={StartupIncubator} alt="sample-logo" />
+      </div>
+    </a>
     <div className={styles["image-wrapper"]}>
       <img className={styles["image"]} src={Partner1} alt="sample-logo" />
     </div>
