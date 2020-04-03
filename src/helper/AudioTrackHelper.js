fe712ad214c3d90eeb4ff72fdfd3daf0516e7676
eddie <eddimuss@web.de>
2020-04-03 20:14:26 +0200

    handling without intro

--- src/helper/AudioTrackHelper.js
+++ src/helper/AudioTrackHelper.js
@@ -1,11 +1,15 @@
 export const createAudioTrackListFromExpertStatements = (expertStatements, panelTitle) => {
-  const audioTrackList = expertStatements.map((statement) => ({
-    audioFile: statement.statement_audio_file.file_link,
-    author: statement.user.full_name,
-    statementId: statement.statement.id,
-    intro: statement.intro.audio_file_link,
-    panelTitle,
-  }));
+  const audioTrackList = expertStatements.map((statement) => {
+    let intro;
+    if (statement.intro) intro = statement.intro.audio_file_link;
+    return {
+      audioFile: statement.statement_audio_file.file_link,
+      author: statement.user.full_name,
+      statementId: statement.statement.id,
+      intro,
+      panelTitle,
+    };
+  });
   return audioTrackList;
 };
 
