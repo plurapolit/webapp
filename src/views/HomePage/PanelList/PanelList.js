24bc3e7911dc6093b66c51884a988b509913ab75
eddie <eddimuss@web.de>
2020-04-16 22:01:57 +0200

    set a limit for rendered panel cards if not all are allowed to render

--- src/views/HomePage/PanelList/PanelList.js
+++ src/views/HomePage/PanelList/PanelList.js
@@ -1,12 +1,14 @@
 /* eslint-disable camelcase */
-import React from "react";
+import React, { useState } from "react";
+import { If } from "react-if";
+import Button, { ButtonStyle } from "../../../components/Button/Button";
 
 import styles from "./PanelList.module.scss";
 import PanelCard from "../PanelCard/PanelCard";
 
-const PanelList = ({ data }) => {
+const getPanelCardFromArray = (array) => {
   const URL = process.env.REACT_APP_BUCKET_URL;
-  const list = data.map(({ panel, experts, panel_avatar }) => (
+  const list = array.map(({ panel, experts, panel_avatar }) => (
     <PanelCard
       key={panel.id}
       title={panel.title}
@@ -17,8 +19,39 @@ const PanelList = ({ data }) => {
       experts={experts}
     />
   ));
+  return list;
+};
 
-  return <div className={styles["panel-list"]}>{list}</div>;
+const MAX_PANELCARDS = 3;
+
+const PanelList = ({ data }) => {
+  const [showAll, setShowAll] = useState(false);
+
+  let list;
+  if (showAll) list = getPanelCardFromArray(data);
+  if (!showAll) list = getPanelCardFromArray(data.slice(0, MAX_PANELCARDS));
+
+  return (
+    <>
+      <div className={styles["panel-list"]}>{list}</div>
+      <If condition={!showAll && data.length > MAX_PANELCARDS}>
+        <Button
+          onClick={() => setShowAll(true)}
+          buttonStyle={ButtonStyle.NONE}
+        >
+          alle Anzeigen
+        </Button>
+      </If>
+      <If condition={showAll}>
+        <Button
+          onClick={() => setShowAll(false)}
+          buttonStyle={ButtonStyle.NONE}
+        >
+          weniger Anzeigen
+        </Button>
+      </If>
+    </>
+  );
 };
 
 export default PanelList;
