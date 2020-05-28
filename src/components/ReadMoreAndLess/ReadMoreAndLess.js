60545cb200bb2dd76daeb0094adf96a00f32a26f
eddie <eddimuss@web.de>
2020-05-28 17:35:44 +0200

    update read more logic

--- src/components/ReadMoreAndLess/ReadMoreAndLess.js
+++ src/components/ReadMoreAndLess/ReadMoreAndLess.js
@@ -1,24 +1,44 @@
 import React from "react";
 import { pipe, createFunction } from "../../helper/FunctionalProgrammingHelper";
 import { getLengthOfString } from "../../helper/StringHelper";
+import Button from "../Button/Button";
+
+const wrapInDiv = (elements) => <div>{elements}</div>;
 
 const ReadMoreAndLess = ({
   readMoreText = "mehr lesen",
   readLessText = "weniger lesen",
-  charLimit = 250,
+  charLimit = 10,
   text,
 }) => {
-  const isLengthLessThanCharLimit = createFunction((length) => length < charLimit);
-  const textIsLessThanCharLimit = pipe(
+  const isLengthMoreThanCharLimit = createFunction((length) => length > charLimit);
+  const textIsMoreThanCharLimit = pipe(
     getLengthOfString,
-    isLengthLessThanCharLimit,
+    isLengthMoreThanCharLimit,
   )(text);
-  console.log("textIsLessThanCharLimit ", textIsLessThanCharLimit);
-  return (
-    <div>
-      Hello
-    </div>
-  );
+
+  if (textIsMoreThanCharLimit) {
+    const addDotts = (string) => `${string} ...`;
+    const getSubstringByCharLimit = (string) => string.substring(0, charLimit);
+    const addLessButton = (elements) => (
+      <>
+        {elements}
+        <Button>
+          {readLessText}
+        </Button>
+      </>
+    );
+    const elements = pipe(
+      getSubstringByCharLimit,
+      addDotts,
+      wrapInDiv,
+      addLessButton,
+    )(text);
+    console.log("elements ", elements);
+    return elements;
+  }
+
+  return text;
 };
 
 export default ReadMoreAndLess;
